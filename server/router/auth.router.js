e81dedd8fc4849c612f0b7cab19957db94ddb4db
KyryloPikhno <Mr.Good@i.ua>
2023-01-02 03:22:39 +0200

    added route

--- server/router/auth.router.js
+++ server/router/auth.router.js
@@ -1,20 +1,27 @@
-const router = require("express").Router()
-const User = require("../dataBase/User")
+const router = require("express").Router();
+const crypto = require("crypto-js");
+
+const User = require("../dataBase/User");
+require('dotenv').config();
 
 
 router.post('/register', async (req, res) => {
-
+    try {
     const newUser = await User.create({
         name:req.body.name,
         email:req.body.email,
-        password:req.body.password
+        password: crypto.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET_WORD).toString(),
     })
-    try {
-        // const savedUser = await newUser.create()
         res.status(201).json(newUser);
     } catch (e) {
         res.status(500).json(e)
     }
 });
+
+router.post('/login', async (req, res) => {
+
+
+});
+
 
 module.exports = router;
