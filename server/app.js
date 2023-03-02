a3fd5edcd68a240f4540d45d3b4a8873ac97d70a
KyryloPikhno <Mr.Good@i.ua>
2023-03-02 19:04:43 +0200

    added PrivateRoute.js

--- server/app.js
+++ server/app.js
@@ -39,15 +39,15 @@ app.use('/devices', deviceRouter);
 app.use('/order', orderRouter);
 
 app.get('/', (req, res) => {
-    res.json('WELCOME')
+    res.json('WELCOME');
 });
 
 app.listen(config.PORT, async () => {
     try {
-        await mongoose.set('strictQuery', false)
+        await mongoose.set('strictQuery', false);
         await mongoose.connect(`mongodb://localhost:27017/${config.DB_NAME}`);
         console.log(`Server listen ${config.PORT}`);
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
 });
